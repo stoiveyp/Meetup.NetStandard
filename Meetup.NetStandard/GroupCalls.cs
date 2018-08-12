98e0edd32b1bc35231a52926af1063168b50b61d
Steven Pears <steven.pears@live.co.uk>
2018-08-12 11:55:28 +0100

    initial structure

---
+++ Meetup.NetStandard/GroupCalls.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Meetup.NetStandard
+{
+    public class GroupCalls:IMeetupGroups
+    {
+        private readonly MeetupClientOptions _options;
+
+        public GroupCalls(MeetupClientOptions options)
+        {
+            _options = options;
+        }
+
+
+    }
+}
