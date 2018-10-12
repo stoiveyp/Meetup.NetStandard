26dcf38b50eec7393bb9d0219d07b03ae3675263
Paul Ballew <34657440+pballewsa@users.noreply.github.com>
2018-10-12 10:30:10 -0600

    Added Group.Find and members property to Group (#11)
    * Added Group.Find
    
    * Added members property to MeetupGroup

--- Meetup.NetStandard/IMeetupGroups.cs
+++ Meetup.NetStandard/IMeetupGroups.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Meetup.NetStandard.Response;
 using Meetup.NetStandard.Response.Groups;
@@ -8,5 +9,6 @@ namespace Meetup.NetStandard
     public interface IMeetupGroups
     {
         Task<MeetupResponse<Group>> Get(string groupName);
+        Task<MeetupResponse<List<Group>>> Find();
     }
 }
